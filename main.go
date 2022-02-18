4b009c60d3d3af3974050c121e93feb8779d6582
taniguchi.kosuke <taniguchi.kosuke@revorn.co.jp>
2022-02-18 18:20:21 +0900

    [fix]main.go

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"GoCleanArch/handler"
+	"GoCleanArch/injector"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	todoHandler := injector.InjectTodoHandler()
+	r := gin.Default()
+	handler.InitRouting(r, todoHandler)
+	r.Run(":8000")
+}
